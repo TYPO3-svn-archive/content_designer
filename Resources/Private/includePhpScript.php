e082932179fdef034a5252517f47bca5fe9fe9d9
kern23 <kern23@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-08-27 12:50:54 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/content_designer/trunk@86239 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Private/includePhpScript.php
+++ Resources/Private/includePhpScript.php
@@ -2,9 +2,13 @@
 
 class contentdesigner_includescript {
   public function main($content, $conf) {
-    ob_start();
-    include_once($GLOBALS['TSFE']->cObj->cObjGetSingle($conf['includeScript'], $conf['includeScript.']));
-    return ob_get_clean();
+	$f = $GLOBALS['TSFE']->cObj->cObjGetSingle($conf['includeScript'], $conf['includeScript.']);
+	
+	if ( file_exists($f) ) {
+		ob_start();
+		include($f);
+		return ob_get_clean();
+	}
   }
 }
 
